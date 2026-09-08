e8cef0f1971543682edf13242b36553f3afa0f2b
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2026-09-08 11:08:03 +0700

    add cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-
-  nitro: {
-    preset: "node-server",
-  },
-
+  tanstackStart: { server: { entry: "server" } },
+  nitro: { preset: "node-server" },
   vite: {
     server: {
       proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-        },
+        "/api": { target: "http://103.147.122.13:5000", changeOrigin: true, secure: false },
       },
     },
   },
